78b233cb99c45b22476484cf09b44068be686c23
Trovs <trovoada360@gmail.com>
2026-02-27 23:28:57 +0000

    I added the two databases URLs

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:igor100@localhost:3306/finlab_db"
+# ===== LOCAL PC =====
+#DATABASE_URL = "mysql+pymysql://root:igor100@localhost:3306/finlab_db"
+
+# ===== CODESPACE =====
+DATABASE_URL=mysql+pymysql://root:root@db:3306/finsys
+
 
 engine = create_engine(DATABASE_URL)
 
